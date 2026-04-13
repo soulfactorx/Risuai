455ef08e2ef101c281208c6b72f220ee0df4bb70
soulfactorx <30439935+soulfactorx@users.noreply.github.com>
2026-04-13 11:30:28 +0900

    Initialize Tauri application with plugins and handlers

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,39 @@
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    let mut builder = tauri::Builder::default();
+
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }));
+    }
+
+    builder
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_deep_link::init())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_fs::init())
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            native_request,
+            check_auth,
+            check_requirements_local,
+            install_python,
+            install_pip,
+            post_py_install,
+            run_py_server,
+            install_py_dependencies,
+            streamed_fetch,
+            oauth_login
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application")
+}
